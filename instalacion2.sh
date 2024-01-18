bd674aeb07e6574ff8262ddadc0899b20d40bd6d
Juanj Sanchez <juanjosesanchezgil@gmail.com>
2024-01-18 18:11:26 +0100

    change

--- instalacion2.sh
+++ instalacion2.sh
@@ -6,41 +6,10 @@ pacman -Sy
 pacman -S alacritty docker docker-compose efibootmgr git grub i3-wm lightdm lightdm-gtk-greeter networkmanager os-prober pulseaudio xorg --noconfirm
 
 
-echo -------------------------
-echo Opciones de usuario
-echo -------------------------
-
-echo -------------------------
-echo Clave usuario root
-echo -------------------------
-
-passwd
-
-echo -------------------------
-echo Introduce nombre de usuario
-echo -------------------------
-
-read usuario
-
-
-useradd -m $usuario
-
-echo -------------------------
-echo Clave usuario $usuario
-echo -------------------------
-
-passwd $usuario
-
 usermod -aG wheel,audio,video,storage,docker $usuario
 
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
 
-read -p "Introduce el nombre de tu host " host
-
-echo $host >> /etc/hostname
-echo "127.0.0.1 localhost
-::1 localhost
-127.0.1.1 $host " >> /etc/hosts
 
 echo -------------------------
 echo Activar servicios
@@ -58,11 +27,6 @@ systemctl enable docker.service
 #alacritty -e git clone https://github.com/juanjosesanchezgil/arch.git
 #chmod a+x arch/postinstalacion.sh
 #alacritty -e ./arch/postinstalacion.sh" >> /home/"$usuario"/.xprofile
-
-
-chown -R $usuario /home/$usuario/
-chgrp -R $usuario /home/$usuario/
-
 
 
 
